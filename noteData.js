ed2d279c6d1f88e0d7d29d9cdd0d8f84baebfece
Christy <mianmianguo2020@gmail.com>
2020-05-16 16:16:24 -0400

    s

---
+++ noteData.js
@@ -0,0 +1,12 @@
+var noteArray = [
+    {
+      noteTitle: "shoppinglist",
+      noteText: "buybanana",
+
+    }
+  ];
+  
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = noteArray;
+  
