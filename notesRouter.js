e0027f8a651da10603ee605d93df253216c8e3c6
Christy <mianmianguo2020@gmail.com>
2020-05-16 20:03:45 -0400

    m

--- notesRouter.js
+++ notesRouter.js
@@ -1,5 +1,36 @@
+var fs = require("fs")
+
 
 module.exports = function (app) {
+    app.get("/api/notes", function (req, res) {
+        fs.readFile(__dirname + "/db/db.json", function (err, data) {
+            if (err) throw err;
+            // console.log(JSON.parse(data))
+            res.json(JSON.parse(data));
+
+        })
+    })
+    app.post("/api/notes", function(request,res) {
+        const data = request.body;
+        console.log(request.body)
+        console.log(request.query)
+        console.log(request.params)
+        // response.json(JSON.stringify(request));
+        // console.log(response)
+        // fs.writeFile(__dirname + "/db/db.json", JSON.stringify(request.body), (err)=> {
+        //     if (err) throw err;
+        //     console.log(data)
+        //     res.json(JSON.parse(data));
+
+        // })
+    })
+    //  app.post("/api/notes", function (req,res) {
+
+
+
+    //  })data
+
+
 
 
 }
--- start.js
+++ start.js
@@ -1,13 +1,15 @@
+var path = require("path");
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080 ; 
-require("./indexRouter")(app);
-require("./notesRouter")(app);
-
-
-
-app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+require("./notesRouter")(app);
+require("./indexRouter")(app);
+
+
+
+// app.use(express.static(path.join(__dirname,"public")));
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
